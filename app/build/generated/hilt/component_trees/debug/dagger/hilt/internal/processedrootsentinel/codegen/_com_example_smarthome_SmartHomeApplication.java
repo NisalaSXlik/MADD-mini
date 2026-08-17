a45c9f312db614533a05f523a57b7910574ccfd8
NisalaSXlik <rashmikanisal46@gmail.com>
2026-08-17 14:25:57 +0530

    chore: remove temporary build files from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_smarthome_SmartHomeApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.smarthome.SmartHomeApplication"
-)
-public final class _com_example_smarthome_SmartHomeApplication {
-}
